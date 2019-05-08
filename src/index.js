6c5fd9fb82c8e09d874cea7e6fbde64f9f687dce
Ana Piazza <piazzaana@fullsail.edu>
2019-05-08 13:14:32 -0400

    little library app using react components

--- src/index.js
+++ src/index.js
@@ -1,44 +1,67 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-let skiData = {
-    total: 50,
-    powder: 20,
-    backcountry: 10,
-    goal: 100
-}
+let bookList = [
+    {"title":"some book", "author":"some author", "pages":300},
+    {"title":"some book", "author":"some author", "pages":200},
+    {"title":"some book", "author":"some author", "pages":400},
+]
 
-const getPercent = decimal => {
-    return decimal * 100 + '%'
-}
-const calcGoalProgress = (total, goal) => {
-    return getPercent(total/goal)
-}
-
-const SkiDayCounter = ({total, powder, backcountry, goal}) => {
-    return (
+const Book = ({title, author, pages, freeBookmark}) =>{
+    return(
         <section>
-                <div>
-                    <p>Total Days: {total}</p>
-                </div>
-                <div>
-                    <p>Powder Days: {powder}</p>
-                </div>
-                <div>
-                    <p>Backcountry Days: {backcountry}</p>
-                </div>
-                <div>
-                    <p>Goal Progress: {calcGoalProgress(total,goal)}</p>
-                </div>
-            </section>
+            <h2>{title}</h2>
+            <p>by: {author}</p>
+            <p>Pages: {pages} pages</p>
+            <p>Free Bookmark Today: {freeBookmark ? 'yes!' : 'no!' }</p>
+        </section>
     )
 }
 
+const Hiring = () => 
+    <div>
+        <p>The library is hiring. Go to www.library.com/jobs for more.</p>
+    </div>
+
+const NotHiring = () => 
+    <div>
+        <p>The library is not hiring. Check back later for more info.</p>
+    </div>
+
+class Library extends Component{
+    
+    state = { 
+        open: true,
+        freeBookmark: true,
+        hiring: true
+    }
+    
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open
+        }))
+    }
+    render(){
+        const { books } = this.props
+        return(
+            <div>
+                {this.state.hiring ? <Hiring /> : <NotHiring />}
+                <h1>The library is {this.state.open ? 'open' : 'closed'}</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
+                {books.map(
+                    (book, i) => <Book 
+                        key={i}
+                        title={book.title} 
+                        author={book.author} 
+                        pages={book.pages}
+                        freeBookmark={this.state.freeBookmark} />
+                )}
+            </div>
+        )
+    }
+}
+
 render(
-    <SkiDayCounter 
-        total={skiData.total}
-        powder={skiData.powder}
-        backcountry={skiData.backcountry}
-        goal={skiData.goal} />,
+    <Library books={bookList} />,
     document.getElementById('root')
 )

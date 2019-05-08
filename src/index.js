b0a184ec743dcaaf72dad645a96835d90feba322
Ana Piazza <piazzaana@fullsail.edu>
2019-05-08 12:00:18 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let skiData = {
+    total: 50,
+    powder: 20,
+    backcountry: 10,
+    goal: 100
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const getPercent = decimal => {
+    return decimal * 100 + '%'
+}
+const calcGoalProgress = (total, goal) => {
+    return getPercent(total/goal)
+}
+
+const SkiDayCounter = ({total, powder, backcountry, goal}) => {
+    return (
+        <section>
+                <div>
+                    <p>Total Days: {total}</p>
+                </div>
+                <div>
+                    <p>Powder Days: {powder}</p>
+                </div>
+                <div>
+                    <p>Backcountry Days: {backcountry}</p>
+                </div>
+                <div>
+                    <p>Goal Progress: {calcGoalProgress(total,goal)}</p>
+                </div>
+            </section>
+    )
+}
+
+render(
+    <SkiDayCounter 
+        total={skiData.total}
+        powder={skiData.powder}
+        backcountry={skiData.backcountry}
+        goal={skiData.goal} />,
+    document.getElementById('root')
+)
